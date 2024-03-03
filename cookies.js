78179e26df310ecc64d25c6391cbdc72aa3f63d7
Andrey <poiprizamoda@gmail.com>
2024-03-03 20:20:34 +0300

    feat: browser JS

---
+++ cookies.js
@@ -0,0 +1,24 @@
+function setCookie(name, value, days) {
+    const expires = new Date();
+    expires.setTime(expires.getTime() + days * 24 * 60 * 60 * 1000);
+    document.cookie = `${name}=${value};expires=${expires.toUTCString()};path=/`;
+}
+
+function getCookie(name) {
+    const cookies = document.cookie.split(';');
+    console.log(cookies)
+    for (let cookie of cookies) {
+        const [cookieName, cookieValue] = cookie.trim().split('=');
+        if (cookieName === name) {
+            return cookieValue;
+        }
+    }
+    return null;
+}
+
+setCookie('username', 'John', 7);
+
+const username = getCookie('username');
+console.log('Username:', username);
+
+setCookie('username', 'Alice', 7);
--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-window.addEventListener('load', function () {
-    console.log(`
+console.log(`
    ╔══════════════════════╗
    ║                      ║
    ║        Senla!        ║
    ║                      ║
    ╚══════════════════════╝
 `);
-
-    console.log(`
+console.log(`
     __  __     ____         _       __           __    __
    / / / /__  / / /___     | |     / /___  _____/ /___/ /
   / /_/ / _ \\/ / / __ \\    | | /| / / __ \\/ ___/ / __  / 
@@ -16,241 +14,65 @@ window.addEventListener('load', function () {
                                                          
                                                                  `)
 
-    // Arithmetic operations
+const alertBtn = document.querySelector('#h1 button');
+alertBtn.addEventListener('click', () => {
+    alert('Alert!')
+})
 
-    const arithmeticObj = {};
 
-    let addition = 1 + 2;
-    let subtraction = 3 - 4;
+const localStorageTextarea = document.querySelector('.localStorage textarea')
+const localStorageBtn = document.querySelector('.localStorage button')
 
-    arithmeticObj['addition'] = addition;
-    arithmeticObj['subtraction'] = subtraction;
-    arithmeticObj['multiplication'] = 5 * 6;
-    arithmeticObj['exponentiation'] = 5 ** 6;
-    arithmeticObj['division'] = 7 / 8;
-    arithmeticObj['remainder of the division'] = 9 % 3;
-    arithmeticObj['increment'] = addition++;
-    arithmeticObj['decrement'] = subtraction--;
-    arithmeticObj['unary plus'] = +true;
+const textLocal = localStorage.getItem('text');
+if (textLocal) {
+    localStorageTextarea.value = textLocal;
+}
+localStorageBtn.addEventListener('click', () => {
+    const text = localStorageTextarea.value;
+    localStorage.setItem('text', text)
+})
 
-    console.log(arithmeticObj)
 
-    // Working with strings
+const sessionStorageTextarea = document.querySelector('.sessionStorage textarea')
+const sessionStorageBtn = document.querySelector('.sessionStorage button')
 
-    const stringObj = {};
+const textSession = sessionStorage.getItem('text');
+if (textSession) {
+    sessionStorageTextarea.value = textSession;
+}
+sessionStorageBtn.addEventListener('click', () => {
+    const text = sessionStorageTextarea.value;
+    sessionStorage.setItem('text', text)
+})
 
-    const hello = 'Hello'
 
-    stringObj['concat'] = 'And' + 'rey'
-    stringObj['double single'] = 'Hello "world"'
-    stringObj['double double'] = "Hello \"world\""
-    stringObj['concat with const'] = hello + ' ' + 'world'
-    stringObj['template strings'] = `${hello} world`
+const buttonURL = document.getElementById('buttonUrl');
+const spanURL = document.getElementById('spanUrl');
+buttonURL.addEventListener('click', () => {
+    spanURL.innerText = `${window.location.href}`
+})
 
-    // Variable assignment
 
-    const a = 1;
-    let b = 2;
-    var c = 3;
-
-    console.log(`
-variableAssignment:
-const a = 1;
-let b = 2;
-var c = 3;
-    `, a, b, c);
-
-    b = 4;
-    c = 6
-
-    console.log(`b = 4,  c = 6:\n`, b, c)
-
-    const assA = a;
-    const assB = b;
-    const assC = c;
-
-    console.log('const assA = a;\nconst assB = b;\nconst assC = c;\n',
-        assA, assB, assC);
-
-    // Console
-
-    console.group('Group Title');
-    console.log('Message 1');
-    console.log('Message 2');
-    console.groupEnd();
-
-    // Logical operation
-
-    const isRegister = true;
-    const orders = 3;
-
-    if (isRegister === true && orders > 0) {
-        console.log(`const isRegister = true;
-const orders = 3;
-if (isRegister === true && orders > 0) {
-    console.log('Success');
-}`);
-        console.log('Success');
+const buttonForward = document.getElementById('goForward');
+const errorForward = document.getElementById('errorForward');
+buttonForward.addEventListener('click', () => {
+    if (window.history.length === 1) {
+        errorForward.innerText = 'Sorry, history length = 1'
+    } else {
+        window.history.forward()
     }
-
-    const html = true;
-    const css = false;
-
-    if (html || css) {
-        console.log(`const html = true;
-const css = false;
-if (html || css) {
-    console.log('JavaScript!')
-}`)
-        console.log('JavaScript!')
-    }
-
-    const programmer = true;
-
-    if (programmer) console.log(`const programmer = true;
-    if (programmer) console.log("You are programmer")`)
-    console.log("You are programmer")
-
-    // try catch
-
-    function someFunction() {
-        const randomNumber = Math.random();
-        if (randomNumber < 0.5) {
-            throw new Error("Error: random number is less than 0.5");
-        }
-        return randomNumber;
-    }
-
-    try {
-        const result = someFunction();
-        console.log("Result:", result);
-    } catch (error) {
-        console.error("An error has occurred:", error);
-    } finally {
-        console.log("Completing error processing");
-    }
-
-    // Arrow Function, default parameters
-
-    const sum = (a = 2, b = 2) => {
-        return a + b;
-    }
-
-    console.log(sum());
-    console.log(sum(1, 1));
-
-    // Spread
-    (function () {
-        const returnMaxNumber = (arr) => {
-            return Math.max(...arr)
-        }
-
-        const arr = [6, 7, 8, 9, 10, 20, 33, 1, 2, 3, 4, 5,]
-
-        console.log(returnMaxNumber(arr))
-    })()
+})
 
 
-    const middle = [2, 3];
-    const arr = [1, ...middle, 4]
-    console.log(arr)
-
-    const str = 'Hello'
-    const chars = [...str]
-    console.log(chars)
-
-    // Rest
-
-    const totalSum = (...values) => {
-        let sum = 0;
-        for (let i = 0; i < values.length; i++) {
-            sum += values[i];
-        }
-        return sum;
-    }
-
-    console.log(totalSum(1, 2, 3, 4, 5, 6, 7, 8, 9));
+const pushToState = document.getElementById('push');
+pushToState.addEventListener('click', () => {
+    window.history.pushState({}, '', './heroPage.html')
+    location.reload();
+})
 
 
-    (function () {
-        const arr = [1, 2, 3, 4, 5, 6];
-
-        const [first, second, ...rest] = arr;
-        console.log(first, second, rest)
-    })();
-
-    // Class, super
-
-    class Parent {
-        constructor(name) {
-            this.name = name;
-        }
-
-        sayHello() {
-            return `Hello ${this.name}`
-        }
-    }
-
-    const parent = new Parent('John');
-    console.log(parent.sayHello())
-
-    class Child extends Parent {
-        constructor(name) {
-            super(name);
-        }
-
-        sayHello() {
-            return `${super.sayHello()} into the Child class`
-        }
-    }
-
-    const child = new Child('Joanna');
-    console.log(child.sayHello())
-
-    // Object destructuring
-
-    const obj = {
-        name: 'John',
-        surname: 'Doe',
-        age: '28'
-    }
-
-    const {name, surname, age} = obj;
-    console.log(name, surname, age + ` y.o.`);
-
-    const {name: firstName, surname: secondName, age: YO} = obj;
-    console.log(firstName,secondName, YO)
-
-    // Yield
-
-    function* generator() {
-        yield 1;
-        yield 2;
-        yield 3;
-    }
-
-    const gen = generator();
-
-    console.log(gen.next().value);
-    console.log(gen.next().value);
-    console.log(gen.next().value);
-
-    (function() {
-        function* fibonacciSequence() {
-            let prev = 1;
-            let curr = 1;
-            while(true) {
-                yield curr;
-                [prev, curr] = [curr, prev + curr];
-            }
-        }
-
-        const gen = fibonacciSequence();
-
-        for (let i = 0; i <= 10; i++) {
-            console.log(gen.next().value)
-        }
-    })()
-
-
+const historyLength = document.getElementById('length');
+const historyLengthSpan = document.getElementById('historyLength');
+historyLength.addEventListener('click', () => {
+    historyLengthSpan.innerText = String(window.history.length);
 })
