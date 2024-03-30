d001aa6c3b4b259312d1484b62c1c2b05bf2c5a5
Andrey <poiprizamoda@gmail.com>
2024-03-30 20:56:03 +0300

    feat: event listeners

--- index.js
+++ index.js
@@ -17,117 +17,161 @@ window.addEventListener('load', function () {
 
     // Arithmetic operations
 
-    const arithmeticObj = {};
+    const arithmeticOperation = () => {
+        const arithmeticObj = {};
 
-    let addition = 1 + 2;
-    let subtraction = 3 - 4;
+        let addition = 1 + 2;
+        let subtraction = 3 - 4;
 
-    arithmeticObj['addition'] = addition;
-    arithmeticObj['subtraction'] = subtraction;
-    arithmeticObj['multiplication'] = 5 * 6;
-    arithmeticObj['exponentiation'] = 5 ** 6;
-    arithmeticObj['division'] = 7 / 8;
-    arithmeticObj['remainder of the division'] = 9 % 3;
-    arithmeticObj['increment'] = addition++;
-    arithmeticObj['decrement'] = subtraction--;
-    arithmeticObj['unary plus'] = +true;
+        arithmeticObj['addition'] = addition;
+        arithmeticObj['subtraction'] = subtraction;
+        arithmeticObj['multiplication'] = 5 * 6;
+        arithmeticObj['exponentiation'] = 5 ** 6;
+        arithmeticObj['division'] = 7 / 8;
+        arithmeticObj['remainder of the division'] = 9 % 3;
+        arithmeticObj['increment'] = addition++;
+        arithmeticObj['decrement'] = subtraction--;
+        arithmeticObj['unary plus'] = +true;
 
-    console.log(arithmeticObj)
+        console.log(arithmeticObj)
+    }
+
+    const arithmeticOperationSpan = document.querySelector('#arithmeticOperation');
+    arithmeticOperationSpan.addEventListener('click', () => {
+        arithmeticOperation();
+    })
 
     // Working with strings
 
-    const stringObj = {};
+    const workingWithStrings = () => {
+        const stringObj = {};
 
-    const hello = 'Hello'
+        const hello = 'Hello'
 
-    stringObj['concat'] = 'And' + 'rey'
-    stringObj['double single'] = 'Hello "world"'
-    stringObj['double double'] = "Hello \"world\""
-    stringObj['concat with const'] = hello + ' ' + 'world'
-    stringObj['template strings'] = `${hello} world`
+        stringObj['concat'] = 'And' + 'rey'
+        stringObj['double single'] = 'Hello "world"'
+        stringObj['double double'] = "Hello \"world\""
+        stringObj['concat with const'] = hello + ' ' + 'world'
+        stringObj['template strings'] = `${hello} world`
+
+        console.log(stringObj)
+    }
+
+    const workingWithStringsSpan = document.querySelector('#workingWithStrings');
+    workingWithStringsSpan.addEventListener('click', () => {
+        workingWithStrings();
+    })
 
     // Variable assignment
 
-    const a = 1;
-    let b = 2;
-    var c = 3;
+    const variableAssigment = () => {
+        const a = 1;
+        let b = 2;
+        var c = 3;
 
-    console.log(`
+        console.log(`
 variableAssignment:
 const a = 1;
 let b = 2;
 var c = 3;
     `, a, b, c);
 
-    b = 4;
-    c = 6
+        b = 4;
+        c = 6
 
-    console.log(`b = 4,  c = 6:\n`, b, c)
+        console.log(`b = 4,  c = 6:\n`, b, c)
 
-    const assA = a;
-    const assB = b;
-    const assC = c;
+        const assA = a;
+        const assB = b;
+        const assC = c;
 
-    console.log('const assA = a;\nconst assB = b;\nconst assC = c;\n',
-        assA, assB, assC);
+        console.log('const assA = a;\nconst assB = b;\nconst assC = c;\n',
+            assA, assB, assC);
+    }
+
+    const variableAssigmentSpan = document.querySelector('#variableAssigment');
+    variableAssigmentSpan.addEventListener('click', () => {
+        variableAssigment();
+    })
 
     // Console
 
-    console.group('Group Title');
-    console.log('Message 1');
-    console.log('Message 2');
-    console.groupEnd();
+    const consoleLog = () => {
+        console.group('Group Title');
+        console.log('Message 1');
+        console.log('Message 2');
+        console.groupEnd();
+    }
+
+    const consoleLogSpan = document.querySelector('#console');
+    consoleLogSpan.addEventListener('click', () => {
+        consoleLog();
+    })
 
     // Logical operation
 
-    const isRegister = true;
-    const orders = 3;
+    const logicalOperation = () => {
+        const isRegister = true;
+        const orders = 3;
 
-    if (isRegister === true && orders > 0) {
-        console.log(`const isRegister = true;
+        if (isRegister === true && orders > 0) {
+            console.log(`const isRegister = true;
 const orders = 3;
 if (isRegister === true && orders > 0) {
     console.log('Success');
 }`);
-        console.log('Success');
-    }
+            console.log('Success');
+        }
 
-    const html = true;
-    const css = false;
+        const html = true;
+        const css = false;
 
-    if (html || css) {
-        console.log(`const html = true;
+        if (html || css) {
+            console.log(`const html = true;
 const css = false;
 if (html || css) {
     console.log('JavaScript!')
 }`)
-        console.log('JavaScript!')
-    }
-
-    const programmer = true;
-
-    if (programmer) console.log(`const programmer = true;
-    if (programmer) console.log("You are programmer")`)
-    console.log("You are programmer")
-
-    // try catch
-
-    function someFunction() {
-        const randomNumber = Math.random();
-        if (randomNumber < 0.5) {
-            throw new Error("Error: random number is less than 0.5");
+            console.log('JavaScript!')
         }
-        return randomNumber;
+
+        const programmer = true;
+
+        if (programmer) console.log(`const programmer = true;
+    if (programmer) console.log("You are programmer")`)
+        console.log("You are programmer")
     }
 
-    try {
-        const result = someFunction();
-        console.log("Result:", result);
-    } catch (error) {
-        console.error("An error has occurred:", error);
-    } finally {
-        console.log("Completing error processing");
+    const logicalOperationSpan = document.querySelector('#logicalOperation');
+    logicalOperationSpan.addEventListener('click', () => {
+        logicalOperation();
+    })
+
+    // Try catch
+
+    const tryCatch = () => {
+        function someFunction() {
+            const randomNumber = Math.random();
+            if (randomNumber < 0.5) {
+                throw new Error("Error: random number is less than 0.5");
+            }
+            return randomNumber;
+        }
+
+        try {
+            const result = someFunction();
+            console.log("Result:", result);
+        } catch (error) {
+            console.error("An error has occurred:", error);
+        } finally {
+            console.log("Completing error processing");
+        }
     }
+
+    const tryCatchSpan = document.querySelector('#tryCatch');
+    tryCatchSpan.addEventListener('click', () => {
+        tryCatch();
+    })
 
 
     // Arithmetics event
