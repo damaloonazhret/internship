382d8eb73c29f27e71c97f8e320d77367a5aed8e
Andrey <poiprizamoda@gmail.com>
2024-03-30 20:30:35 +0300

    feat: arithmetic event

--- index.js
+++ index.js
@@ -12,8 +12,7 @@ window.addEventListener('load', function () {
    / / / /__  / / /___     | |     / /___  _____/ /___/ /
   / /_/ / _ \\/ / / __ \\    | | /| / / __ \\/ ___/ / __  / 
  / __  /  __/ / / /_/ /    | |/ |/ / /_/ / /  / / /_/ /  
-/_/ /_/\\___/_/_/\\____/     |__/|__/\\____/_/  /_/\\__,_/   
-                                                         
+/_/ /_/\\___/_/_/\\____/     |__/|__/\\____/_/  /_/\\__,_/            
                                                                  `)
 
     // Arithmetic operations
@@ -131,4 +130,56 @@ if (html || css) {
     }
 
 
+    // Arithmetics event
+
+    const inputOperands = document.querySelectorAll('#arithmetics input');
+    inputOperands.forEach((el) => {
+        el.addEventListener('input', (e) => {
+            if (e.target.value.length > 4) {
+                e.target.value = e.target.value.slice(0, 4);
+            }
+        })
+    })
+
+    const equal = document.querySelector('#equal');
+    equal.addEventListener('click', () => {
+        const operator = document.querySelector('#operators').value;
+        const solution = document.querySelector('#solution');
+        const inputOperands = document.querySelectorAll('#arithmetics input');
+
+        const firstOperand = +inputOperands[0].value;
+        const secondOperand = +inputOperands[1].value;
+
+        let result;
+
+        switch (operator) {
+            case '+':
+                result = firstOperand + secondOperand;
+                break
+            case '-':
+                result = firstOperand - secondOperand;
+                break
+            case '*':
+                result = firstOperand * secondOperand;
+                break
+            case '/':
+                if (secondOperand === 0) {
+                    result = 'You can\'t divide by zero';
+                } else {
+                    result = firstOperand / secondOperand;
+                }
+                break
+            default:
+                result = 'unknown operation';
+                break
+        }
+
+        if (Number.isInteger(result)) {
+            result = result.toString();
+        } else {
+            result = result.toFixed(2);
+        }
+
+        solution.innerText = result;
+    })
 })
