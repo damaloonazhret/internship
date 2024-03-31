efcf6cea291c2085b4198a3dc5bde706b1e198a7
Andrey <poiprizamoda@gmail.com>
2024-03-31 15:37:24 +0300

    feat: add some functionality to all functions

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ window.addEventListener('load', function () {
 
     // Arithmetic operations
 
-    const arithmeticOperation = () => {
+    function arithmeticOperation() {
+        console.clear();
         const arithmeticObj = {};
 
         let addition = 1 + 2;
@@ -27,90 +28,151 @@ window.addEventListener('load', function () {
         arithmeticObj['subtraction'] = subtraction;
         arithmeticObj['multiplication'] = 5 * 6;
         arithmeticObj['exponentiation'] = 5 ** 6;
+        arithmeticObj['square root'] = 16 ** (1 / 2);
         arithmeticObj['division'] = 7 / 8;
         arithmeticObj['remainder of the division'] = 9 % 3;
-        arithmeticObj['increment'] = addition++;
-        arithmeticObj['decrement'] = subtraction--;
+        arithmeticObj['post increment'] = addition++;
+        arithmeticObj['pre increment'] = ++addition;
+        arithmeticObj['post decrement'] = subtraction--;
+        arithmeticObj['pre decrement'] = --subtraction;
         arithmeticObj['unary plus'] = +true;
 
         console.log(arithmeticObj)
     }
 
     const arithmeticOperationSpan = document.querySelector('#arithmeticOperation');
-    arithmeticOperationSpan.addEventListener('click', () => {
+    arithmeticOperationSpan.addEventListener('click', function () {
         arithmeticOperation();
     })
 
     // Working with strings
 
-    const workingWithStrings = () => {
+    function workingWithStrings() {
+        console.clear();
         const stringObj = {};
 
-        const hello = 'Hello'
+        const hello = 'Hello';
+        const one = '1';
+        const two = '2';
 
-        stringObj['concat'] = 'And' + 'rey'
-        stringObj['double single'] = 'Hello "world"'
-        stringObj['double double'] = "Hello \"world\""
-        stringObj['concat with const'] = hello + ' ' + 'world'
-        stringObj['template strings'] = `${hello} world`
+        stringObj['concat'] = 'And' + 'rey';
+        stringObj['double single'] = 'Hello "world"';
+        stringObj['double double'] = "Hello \"world\"";
+        stringObj['template strings'] = `${hello} world`;
+        stringObj['concat with const'] = hello + ' ' + 'world';
+        stringObj['addition of number strings'] = one + two;
+        stringObj['string conversion '] = +one + +two;
 
-        console.log(stringObj)
+        console.log(stringObj);
     }
 
     const workingWithStringsSpan = document.querySelector('#workingWithStrings');
-    workingWithStringsSpan.addEventListener('click', () => {
+    workingWithStringsSpan.addEventListener('click', function () {
         workingWithStrings();
     })
 
     // Variable assignment
 
-    const variableAssigment = () => {
+    function variableAssigment() {
+        console.clear();
         const a = 1;
-        let b = 2;
+        let b;
+        b = 2;
         var c = 3;
+
+        console.log(`
+const obj = {
+  name: 'Alex',
+  age: 16
+}
+console.log(obj);
+obj.name = 'Denchik';
+obj.age = 18
+console.log(obj);
+        `);
+        const obj = {
+            name: 'Alex',
+            age: 16
+        }
+        console.log(obj);
+        obj.name = 'Denchik';
+        obj.age = 18
+        console.log(obj);
 
         console.log(`
 variableAssignment:
 const a = 1;
-let b = 2;
+let b;
+b = 2;
 var c = 3;
     `, a, b, c);
 
-        b = 4;
-        c = 6
+        b = 'Web';
+        c = '3.0'
 
-        console.log(`b = 4,  c = 6:\n`, b, c)
+        console.log(`b = 'Web',  c = '3.0':\n`, b, c)
 
-        const assA = a;
-        const assB = b;
-        const assC = c;
+        const copyA = a;
+        const copyB = b;
+        const copyC = c;
 
-        console.log('const assA = a;\nconst assB = b;\nconst assC = c;\n',
-            assA, assB, assC);
+        console.log('const copyA = a;\nconst copyB = b;\nconst copyC = c;\n',
+            copyA, copyB, copyC);
     }
 
     const variableAssigmentSpan = document.querySelector('#variableAssigment');
-    variableAssigmentSpan.addEventListener('click', () => {
+    variableAssigmentSpan.addEventListener('click', function () {
         variableAssigment();
     })
 
     // Console
 
-    const consoleLog = () => {
+    function consoleLog() {
+        console.clear();
+        console.time('Execution time took')
         console.group('Group Title');
         console.log('Message 1');
         console.log('Message 2');
         console.groupEnd();
+        console.info('Info');
+        console.error('Error');
+        console.warn('Warn');
+        console.assert(1 === 2, '1 doesn\'t equal 2')
+        console.timeEnd('Execution time took')
     }
 
     const consoleLogSpan = document.querySelector('#console');
-    consoleLogSpan.addEventListener('click', () => {
+    consoleLogSpan.addEventListener('click', function () {
         consoleLog();
     })
 
     // Logical operation
 
-    const logicalOperation = () => {
+    function logicalOperation() {
+        console.clear();
+        let zero = 0;
+        let uno = 1;
+        const stringUno = '1';
+
+        console.log(`
+console.log(
+    zero > uno, 
+    zero < uno, 
+    stringUno == uno, 
+    stringUno === uno, 
+    zero++ <= uno, 
+    zero <= uno--
+);
+`)
+        console.log(
+            zero > uno,
+            zero < uno,
+            stringUno == uno,
+            stringUno === uno,
+            zero++ <= uno,
+            zero <= uno--
+        );
+
         const isRegister = true;
         const orders = 3;
 
@@ -143,13 +205,15 @@ if (html || css) {
     }
 
     const logicalOperationSpan = document.querySelector('#logicalOperation');
-    logicalOperationSpan.addEventListener('click', () => {
+    logicalOperationSpan.addEventListener('click', function () {
         logicalOperation();
     })
 
     // Try catch
 
-    const tryCatch = () => {
+    function tryCatch() {
+        console.clear();
+
         function someFunction() {
             const randomNumber = Math.random();
             if (randomNumber < 0.5) {
@@ -169,16 +233,15 @@ if (html || css) {
     }
 
     const tryCatchSpan = document.querySelector('#tryCatch');
-    tryCatchSpan.addEventListener('click', () => {
+    tryCatchSpan.addEventListener('click', function () {
         tryCatch();
     })
-
 
     // Arithmetics event
 
     const inputOperands = document.querySelectorAll('#arithmetics input');
-    inputOperands.forEach((el) => {
-        el.addEventListener('input', (e) => {
+    inputOperands.forEach(function (el) {
+        el.addEventListener('input', function (e) {
             if (e.target.value.length > 4) {
                 e.target.value = e.target.value.slice(0, 4);
             }
@@ -186,7 +249,7 @@ if (html || css) {
     })
 
     const equal = document.querySelector('#equal');
-    equal.addEventListener('click', () => {
+    equal.addEventListener('click', function () {
         const operator = document.querySelector('#operators').value;
         const solution = document.querySelector('#solution');
         const inputOperands = document.querySelectorAll('#arithmetics input');
