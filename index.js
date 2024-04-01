54374550a1392697f208935cee895c0f4c3f4a74
Andrey <poiprizamoda@gmail.com>
2024-04-01 19:46:57 +0300

    feat: for of, cycles module

--- index.js
+++ index.js
@@ -297,4 +297,33 @@ if (html || css) {
 
         solution.innerText = result;
     })
+
+    // Cycles
+
+    function cycles () {
+        let i = 5;
+        while (i) {
+            i--
+            console.log(i)
+        }
+
+        let j = 0;
+        do {
+            j++
+            if (j === 2) continue
+            console.log(j)
+        } while (j < 4)
+
+        outer: for (let i = 6; i <= 18; i++) {
+            for (let j = 25; j <= 30; j++) {
+                if (i === 7) break outer
+                console.log(`${i}:${j} AM`)
+            }
+        }
+    }
+
+    const cyclesSpan = document.querySelector('#cycles');
+    cyclesSpan.addEventListener('click', function () {
+        cycles();
+    })
 })
