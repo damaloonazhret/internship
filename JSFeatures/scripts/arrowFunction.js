6a78a4aec56daad7e9af2b7b33a0a2debac00862
Andrey <poiprizamoda@gmail.com>
2024-04-08 22:20:25 +0300

    fix: console.log

--- JSFeatures/scripts/arrowFunction.js
+++ JSFeatures/scripts/arrowFunction.js
@@ -72,3 +72,4 @@ console.log(functionDeclaration());
     `)
     arrowFunction();
 })
+
--- scripts/arithmetic.js
+++ scripts/arithmetic.js
@@ -1,7 +1,6 @@
 // Arithmetic operations
 
 const arithmeticOperation = () => {
-    console.clear();
     const arithmeticObj = {};
 
     let additionType = 1 + 2;
@@ -31,5 +30,33 @@ const arithmeticOperation = () => {
 
 const arithmeticOperationSpan = document.querySelector('#arithmeticOperation');
 arithmeticOperationSpan.addEventListener('click', () => {
+    console.clear();
+    console.log(`
+const arithmeticObj = {};
+
+let additionType = 1 + 2;
+let subtractionType = 3 - 4;
+
+arithmeticObj['addition'] = additionType;
+arithmeticObj['subtraction'] = subtractionType;
+arithmeticObj['multiplication'] = 5 * 6;
+arithmeticObj['exponentiation'] = 5 ** 6;
+arithmeticObj['squareRoot'] = 16 ** (1 / 2);
+arithmeticObj['division'] = 7 / 8;
+arithmeticObj['remainderOfTheDivision'] = 9 % 3;
+arithmeticObj['postIncrement'] = additionType++;
+arithmeticObj['preIncrement'] = ++additionType;
+arithmeticObj['postDecrement'] = subtractionType--;
+arithmeticObj['preDecrement'] = --subtractionType;
+arithmeticObj['unaryPlus'] = +true;
+
+// for (const key in arithmeticObj) {
+//     console.log(key, arithmeticObj[key]);
+// }
+
+for (const [key, value] of Object.entries(arithmeticObj)) {
+    console.log(key, value);
+}
+    `)
     arithmeticOperation();
 })
