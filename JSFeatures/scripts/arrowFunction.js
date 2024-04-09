172fe95311c5228445d4e1488fc07dde95bef24a
Andrey <poiprizamoda@gmail.com>
2024-04-09 18:14:02 +0300

    fix: remove unnecessary console.log, simplification of syntactic construction

--- JSFeatures/scripts/arrowFunction.js
+++ JSFeatures/scripts/arrowFunction.js
@@ -36,5 +36,39 @@ const arrowFunction = () => {
 const arrowFunctionSpan = document.querySelector('#arrowFunction');
 arrowFunctionSpan.addEventListener('click', () => {
     console.clear();
+    console.log(`
+const arrow = a => a + 3;
+console.log(arrow(3));
+
+const arrow2 = (a, b) => a + b;
+console.log(arrow2(3, 4));
+
+const arrow3 = (a, b, c, ...rest) => {
+    let sum = 0;
+    sum += a;
+    sum += b;
+    sum += c;
+    for (const arg of rest) {
+        sum += arg;
+    }
+    return sum;
+}
+
+console.log(arrow3(1, 2, 3, 4, 5, 6, 7, 8, 9));
+
+const arrowFunction = () => {
+    return this;
+}
+console.log(arrowFunction() === window);
+
+function functionDeclaration() {
+    this.num = 1;
+    return (() => {
+        return this.num;
+    })();
+}
+
+console.log(functionDeclaration());
+    `)
     arrowFunction();
 })
