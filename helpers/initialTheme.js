8c5a053ba68de50155096945e1c6482a98101532
Andrey <poiprizamoda@gmail.com>
2024-04-14 20:26:53 +0300

    fix: reset button

--- helpers/initialTheme.js
+++ helpers/initialTheme.js
@@ -14,7 +14,7 @@ import {
 const sessionTextColor = sessionStorage.getItem('text');
 const sessionBGColor = sessionStorage.getItem('bg');
 
-function setRootProperty(color, session) {
+export function setRootProperty(color, session) {
     document.documentElement.style.setProperty(color, session)
 }
 
