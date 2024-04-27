40a0a02c08801480a6bc875a94917e645d970cc5
Andrey <poiprizamoda@gmail.com>
2024-04-27 10:09:05 +0300

    fix: react config

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
+import logo from './logo.svg';
 import './App.css';
-import React from "react";
-import AsyncPage from "./pages/AsyncPage";
-import PromisePage from "./pages/PromisePage";
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import Header from "./components/Header/Header";
 
-class App extends React.Component {
-
-    render() {
-        return (
-            <Router>
-                <Header/>
-                <Routes>
-                    <Route exact path="/" element={<AsyncPage/>}/>
-                    <Route path="/about" element={<PromisePage/>}/>
-                </Routes>
-            </Router>
-        )
-    }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
