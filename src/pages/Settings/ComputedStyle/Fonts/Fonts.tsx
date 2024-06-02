a9dc35948009d0369eb92f35ae896b7f345df02a
Andrey <poiprizamoda@gmail.com>
2024-06-02 13:20:26 +0300

    fix: mobile hover none

--- src/pages/Settings/ComputedStyle/Fonts/Fonts.tsx
+++ src/pages/Settings/ComputedStyle/Fonts/Fonts.tsx
@@ -16,14 +16,14 @@ const Fonts: FC<FontsProps> = (props) => {
     <div className="settings__font">
       <p>Value: {props.count.fontSize ? props.count.fontSize : props.FS}px </p>
       <p>Here you can set a custom font size for the entire page</p>
-      <Button className="btn" onClick={props.decrement}>
-          decrease
+      <Button className="btn big" onClick={props.decrement}>
+          Decrease
       </Button>
       <Button className="btn" onClick={props.reset}>
-        reset
+        Reset
       </Button>
-      <Button className="btn" onClick={props.increment}>
-        increase
+      <Button className="btn big" onClick={props.increment}>
+        Increase
       </Button>
     </div>
   );
